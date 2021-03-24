d6b9208a515c3c2e6d3956177954ee450d13d14c
MaironCasa <mairongallas@gmail.com>
2021-03-24 01:54:07 -0300

    Corrigido erros nos Testes

--- estima_fasor_fft/estimador/criar_sinal.py
+++ estima_fasor_fft/estimador/criar_sinal.py
@@ -10,4 +10,10 @@ class SinalTeste():
 
     def criar_sinal(self):
         self.time = np.arange(0, 2 * 1 / self.frequencia, (1 / self.frequencia) / self.amostragem)
-        self.dados = self.amplitude * np.sin((2 * np.pi * 60 * self.time) + self.defasagem)
+        self.dados = self.amplitude * np.sin((2 * np.pi * self.frequencia * self.time) + self.defasagem)
+
+    def criar_sinal_duas_frequencias(self):
+        self.time = np.arange(0, 2 * 1 / self.frequencia, (1 / self.frequencia) / self.amostragem)
+        self.dados = self.amplitude * np.sin((2 * np.pi * self.frequencia * self.time) + self.defasagem) + (
+            (self.amplitude / 2) * np.sin((2 * np.pi * self.frequencia * 3 * self.time))
+        )
--- estima_fasor_fft/tests/test_estimador_de_fasor.py
+++ estima_fasor_fft/tests/test_estimador_de_fasor.py
@@ -4,29 +4,6 @@ from estima_fasor_fft.estimador.criar_fasor import Fasor, Fourier
 from estima_fasor_fft.estimador.criar_sinal import SinalTeste
 
 
-class TransfomadaFourie():
-    def __init__(self, amostragem, frequencia):
-        self.frequencia = frequencia
-        self.amostragem = amostragem
-        self.janela_de_amostras = np.arange(0, self.amostragem, 1)
-        self.cria_coef_sin_cos()
-
-    def cria_coef_sin_cos(self):
-        angle = (2 * np.pi / self.amostragem) * self.janela_de_amostras
-        self.cfc = np.cos(angle)
-        self.cfs = - np.sin(angle)
-
-    def estima_modulo(self, sinal):
-        modulo = np.zeros((len(sinal) - self.amostragem, 1))
-
-        for i in range(self.amostragem, len(sinal), 1):
-            fftR = (np.sum(sinal[i - self.amostragem:i] * self.cfc)) / (2 / self.amostragem)
-            fftI = (np.sum(sinal[i - self.amostragem:i] * self.cfs)) / (2 / self.amostragem)
-            modulo[i - self.amostragem] = np.sqrt(fftR * fftR + (fftI * fftI))
-
-        return modulo
-
-
 @pytest.mark.parametrize(
     'frequencia',
     [60, 180]
@@ -35,9 +12,23 @@ def test_criar_sinal(frequencia):
     amplitude = 10
     amostragem = 128
     defasagem = 0
+
     sinal = SinalTeste(amplitude, defasagem, amostragem, frequencia)
     sinal.criar_sinal()
+
     assert sinal.amostragem == 128
+
+
+def test_criar_sinal_duas_frequencias():
+    amplitude = 10
+    amostragem = 128
+    defasagem = 0
+    frequencia = 60
+
+    sinal = SinalTeste(amplitude, defasagem, amostragem, frequencia)
+    sinal.criar_sinal_duas_frequencias()
+
+    assert sinal.amostragem == amostragem
 
 
 def test_modulo_fasor():
@@ -68,4 +59,21 @@ def test_angulo_fasor():
     fft_cfg = Fourier(sinal)  # Configurações do Fourier
     fasor = Fasor(fft_cfg)
     fasor.estimar()
+
     assert (fasor.fase[0] < 90.1 and fasor.fase[0] > 89.8)
+
+
+@pytest.mark.parametrize(
+    'frequencia , amplitude',
+    [(60, 5), (180, 10)]
+)
+def test_fasores(frequencia, amplitude):
+    amostragem = 128
+    defasagem = 0
+
+    sinal = SinalTeste(amplitude, defasagem, amostragem, frequencia)
+    sinal.criar_sinal_duas_frequencias()
+
+    fft_cfg = Fourier(sinal)  # Configurações do Fourier
+    fasor = Fasor(fft_cfg)
+    fasor.estimar()
